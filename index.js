b6b421bdff733a1fdda7668d549dd99dcdea99c9
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-10-09 21:55:56 +0600

    put the myjewellers

--- index.js
+++ index.js
@@ -70,6 +70,20 @@ async function run() {
         }
       });
 
+      app.put('/api/myJewellers/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateJewellers = req.body;
+        console.log(updateJewellers);
+        const updateDoc = {
+            $set: {
+                status: updateToys.status
+            },
+        };
+        const result = await jewellerCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
