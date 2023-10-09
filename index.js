e833700bec3e6dfacb33154eb414427d143ad5df
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-10-09 20:28:32 +0600

    get the jeweller

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 5000;
 //NQI9M7Ofoavnv1RJ
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Add this line to enable body parsing
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://jewelleryShop:NQI9M7Ofoavnv1RJ@cluster0.s1xtzuq.mongodb.net/?retryWrites=true&w=majority";
-// const uri = "mongodb+srv://<username>:<password>@cluster0.s1xtzuq.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://${}:${process.env.DB_PASS}@cluster0.s1xtzuq.mongodb.net/?retryWrites=true&w=majority";
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1xtzuq.mongodb.net/?retryWrites=true&w=majority`;
 
 // 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +28,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jewellerCollection = client.db('jewelleryShop').collection('jeweller');
+
+    app.get('/jeweller', async (req, res) => {
+        console.log(req.body);
+        let query = {};
+        if (req.query?.email) {
+            query = { email: req.query.email }
+        }
+        const result = await jewellerCollection.find(query).toArray();
+        console.log(result);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
