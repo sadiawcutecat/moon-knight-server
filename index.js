534623e5a82a26bf8fd22c61ed9bb718fcda7c61
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-10-09 20:09:06 +0600

    mongodb connect successfully

--- index.js
+++ index.js
@@ -9,17 +9,44 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://jewelleryShop:NQI9M7Ofoavnv1RJ@cluster0.s1xtzuq.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://<username>:<password>@cluster0.s1xtzuq.mongodb.net/?retryWrites=true&w=majority";
+
+// 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
 run().catch(console.dir);
+
+
+
 
 
  app.get('/', (req, res)=>{
     res.send('server is running')
  })
-//  app.get('/customerComment',(req, res)=>{
-//    res.send(customerComment);
-//   //  console.log(customerComment);
-//  })
+
 
  app.listen(port, ()=>{
-    console.log(`toy server is running: ${port}`);
+    console.log(`jewellery server is running: ${port}`);
  })
